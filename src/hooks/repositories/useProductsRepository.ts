51d98afe01db2d49800fc7a8f3538744a905f61a
Anderson Vargas Sepulveda <andersonvargas383@gmail.com>
2023-05-07 14:27:34 -0500

    add comments functions

--- src/hooks/repositories/useProductsRepository.ts
+++ src/hooks/repositories/useProductsRepository.ts
@@ -2,7 +2,9 @@ import { useRepositoryIoc } from '../../services/config/context';
 import { TYPES } from '../../services/config/types';
 import { ProductsRepository } from '../../services/repositories/products.repository';
 
-
+/**
+   * Custom Hook that return ProductsRepository
+   */
 const useProductsRepository = () : ProductsRepository => {
   const { container } = useRepositoryIoc();
   return container.get(TYPES.PRODUCTS_REPOSITORY);
--- src/services/repositories/products.repository.ts
+++ src/services/repositories/products.repository.ts
@@ -1,6 +1,16 @@
 import { IProduct } from "../models/product";
 
 export interface ProductsRepository {
+
+  /**
+   * Get list elements
+   * @param {string} code A code category filter
+   */
   list (code:string): Promise<IProduct[]>
+
+  /**
+   * Get One element
+   * @param {string} id A id product
+   */
   get (id:string): Promise<IProduct> 
 }
